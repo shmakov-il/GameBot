08d3bf6aef90cee97fca5cad0636745b2a13ed26
Shmakov Ilya <ilia.eduardovich98@gmail.com>
2021-02-18 16:00:59 +0300

    feat(main.js): add lesson OOP

--- main.js
+++ main.js
@@ -1,32 +1,47 @@
+
 'use strict';
 
-const gameBot = function () {
-  const targetNumber = 43;
-  const number = function () {
+function DomElement(selector, height, width, bg, fontSize) {
+  this.selector = prompt('Напишите ".<ваше слово>" или "#<ваше слово>"');
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+}
 
-    let isNumber = function(n) {
-      return !isNaN(parseFloat(n)) && isFinite(n);
-    };
-
-    const enterNumber = prompt('Угадайте число от 1 до 100?');
-      if (enterNumber === null) {
-      alert('Игра окончена!'); 
-    } else if (!isNumber(enterNumber)) {
-      alert('Введите число!');
-      number();
-    } else if (Number(enterNumber) > targetNumber) {
-      alert('Загаданное число меньше!');
-      number();
-    } else if (Number(enterNumber) < targetNumber) {
-      alert('Загаданное число больше!');
-      number();
-    } else if (Number(enterNumber) === targetNumber) {
-      alert('Поздравляю, вы победили!');
-    } 
-  };
-  number();
+DomElement.prototype.createElement = function () {
+  if (this.selector.startsWith('.')) {
+      this.selector = this.selector.substring(1);
+      let divElement = document.createElement('div');
+      document.body.append(divElement);
+      divElement.classList.add(`${this.selector}`);
+      divElement.textContent = 'Я - блок';
+      this.selector = `.${this.selector}`;
+  } else if (this.selector.startsWith('#')) {
+      this.selector = this.selector.substring(1);
+      let pElement = document.createElement('p');
+      document.body.append(pElement);
+      pElement.classList.add(`${this.selector}`);
+      pElement.textContent = 'Я - параграф';
+      this.selector = `#${this.selector}`;
+  }
 };
-gameBot();
+
+DomElement.prototype.cssText = function () {
+  let styleForElement = document.querySelector(`${this.selector}`);
+styleForElement.style.cssText = `
+height: ${this.height};
+width: ${this.width};
+background-color: ${this.bg};
+font-size: ${this.fontSize};
+`;
+};
+
+const newElement = new DomElement ('.block', '200px', '200px', 'red', '20px');
+
+newElement.createElement();
+newElement.cssText();
+
 
 
 
